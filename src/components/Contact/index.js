fa1423225919030bcb61c1e70c74b1459446701c
ancs214 <ancs214@gmail.com>
2022-06-15 19:56:01 -0400

    synced data from the contact form to the state of the component.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 function ContactForm() {
-    const [formState, setFormState] = useState();
+    
+    // we want to clear the input fields of name, email, message e4t on component loading
+    const [formState, setFormState] = useState({name: '', email: '', message: ''});
+
+    const { name, email, message } = formState;
+
+
+    function handleChange(e) {
+        // The name property of target in the preceding expression actually refers to the name attribute of the form element. This attribute value matches the property names of formState (name, email, and message) and allows us to use [ ] to create dynamic property names.
+        setFormState({...formState, [e.target.name]: e.target.value })
+      }
+      
+      console.log(formState);
+      
     return (
         <section>
             <h1>Contact me</h1>
@@ -9,15 +22,15 @@ function ContactForm() {
                 <div>
                     {/* htmlFor is same as 'for' attribute in JS */}
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name"  />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea defaultValue={message} name="message" onChange={handleChange} rows="5"  />
                 </div>
                 <button type="submit">Submit</button>
             </form>
